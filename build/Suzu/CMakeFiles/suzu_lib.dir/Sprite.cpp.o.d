fa3e0e5dab76110cfe0b829ced82951ee1a2d2cc
ef1301 <fangemily60@gmail.com>
2021-12-22 19:55:35 -0500

    updated gitignore

--- build/Suzu/CMakeFiles/suzu_lib.dir/Sprite.cpp.o.d
+++
@@ -1,9 +0,0 @@
-Suzu/CMakeFiles/suzu_lib.dir/Sprite.cpp.o: \
- /home/emilyf/F21_Emily_Fang/Suzu/Sprite.cpp /usr/include/stdc-predef.h \
- /home/emilyf/F21_Emily_Fang/Suzu/Sprite.h \
- /home/emilyf/F21_Emily_Fang/Suzu/SuzuUtils.h \
- /home/emilyf/F21_Emily_Fang/Suzu/SpriteImplementation.h \
- /home/emilyf/F21_Emily_Fang/Suzu/OpenGLimpl/OpenGLSprite.h \
- /home/emilyf/F21_Emily_Fang/Suzu/../glad/include/glad/glad.h \
- /home/emilyf/F21_Emily_Fang/Suzu/../glad/include/KHR/khrplatform.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/11.1.0/include/stdint.h
--- build/stbi/CMakeFiles/stbi.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for stbi.
-# This may be replaced when dependencies are built.
